1c924a0cc363a59533be47bdc961187fc300fd3a
Ayan <sujon.seo.bd@gmail.com>
2026-09-21 14:40:43 +0600

    update: updated otp screen

--- src/app/(auth)/register/verify-account/page.tsx
+++ src/app/(auth)/register/verify-account/page.tsx
@@ -38,31 +38,14 @@ import { Suspense } from "react";
 
 export default function VerifyAccountPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <Link href="/" className="flex items-center gap-2 font-medium">
-            <div className="flex items-center gap-2">
-              {/* <Logo /> */}
-              <span>CampusFlow</span>
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
+    <div className="grid min-h-screen ">
+        <div className="flex flex-1 items-center justify-center m-2">
+          <div className="w-full max-w-sm">
             <Suspense fallback={<p>Loading...</p>}>
               <VerifyAccountForm/>
             </Suspense>
           </div>
         </div>
-      </div>
-      {/* <div className="relative hidden bg-muted lg:block">
-        <Image
-          src="/images/login-image.jpg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
-      </div> */}
     </div>
   );
 }
